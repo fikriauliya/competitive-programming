8d1c98527278ed1891b3f3f4fb1c9a8312197a60
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2020-10-13 22:43:28 +0700

    Add pokerhand

---
+++ Kattis/pokerhand/src/main.rs
@@ -0,0 +1,29 @@
+use std::io;
+use std::str;
+use std::convert::TryFrom;
+use std::collections::HashMap;
+
+pub struct UnsafeScanner<R> { reader: R, buf_str: Vec<u8>, buf_iter: str::SplitAsciiWhitespace<'static>, }
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self { Self { reader, buf_str: vec![], buf_iter: "".split_ascii_whitespace(), } }
+    pub fn trim_newline(s: &mut String) { if s.ends_with('\n') { s.pop(); if s.ends_with('\r') { s.pop(); } } }
+    pub fn token<T: str::FromStr>(&mut self) -> Option<T> { loop { if let Some(token) = self.buf_iter.next() { return Some(token.parse().ok().expect("Failed parse")); } self.buf_str.clear(); let len = self.reader .read_until(b'\n', &mut self.buf_str) .expect("Failed read"); if len == 0 { return None; } self.buf_iter = unsafe { let slice = str::from_utf8_unchecked(&self.buf_str); std::mem::transmute(slice.split_ascii_whitespace()) } } }
+    pub fn line(&mut self) -> Option<String> { let mut input = String::new(); let len = self.reader.read_line(&mut input).expect("Failed read"); match len { 0 => None, _ => { Self::trim_newline(&mut input); Some(input) } } }
+}
+fn main() { let (stdin, stdout) = (io::stdin(), io::stdout()); let mut scan = UnsafeScanner::new(stdin.lock()); let mut out = io::BufWriter::new(stdout.lock()); solve(&mut scan, &mut out); }
+
+fn solve<R: io::BufRead, W: io::Write>(scan: &mut UnsafeScanner<R>, out: &mut W) {
+    let line = scan.line().unwrap();
+    let cards:Vec<&str> = line.split_ascii_whitespace().collect();
+    let mut map = HashMap::new();
+    for card in cards {
+        let rank = card.as_bytes()[0] as char;
+        let ctr = map.get_mut(&rank);
+        if let Some(ctr) = ctr {
+            *ctr = *ctr + 1;
+        } else {
+            map.insert(rank, 1);
+        }
+    }
+    writeln!(out, "{}", map.values().max().unwrap());
+}
