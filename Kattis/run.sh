be73f095d7511b87c173a549c3bbaf84b3b47a35
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2020-10-13 10:12:13 +0700

    Move run.sh and generator to Makefile

--- Kattis/run.sh
+++
@@ -1,4 +0,0 @@
-project="$1"
-testcase="$2"
-RUST_BACKTRACE=1 cargo run -p $project --bin $project < $project/$testcase.in >$project/$testcase.out
-diff -y --left-column $project/$testcase.ans $project/$testcase.out
