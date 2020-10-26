24e768f3ccb9e87e1d08297c9a16764a80be18cd
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2020-10-27 06:11:38 +0700

    Add chartingprogress

---
+++ Kattis/chartingprogress/src/main.rs
@@ -0,0 +1,34 @@
+use std::io;
+use std::str;
+use std::convert::TryFrom;
+
+pub struct UnsafeScanner<R> { reader: R, buf_str: Vec<u8>, buf_iter: str::SplitAsciiWhitespace<'static>, }
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self { Self { reader, buf_str: vec![], buf_iter: "".split_ascii_whitespace(), } }
+    pub fn trim_newline(s: &mut String) { if s.ends_with('\n') { s.pop(); if s.ends_with('\r') { s.pop(); } } }
+    pub fn token<T: str::FromStr>(&mut self) -> Option<T> { loop { if let Some(token) = self.buf_iter.next() { return Some(token.parse().ok().expect("Failed parse")); } self.buf_str.clear(); let len = self.reader .read_until(b'\n', &mut self.buf_str) .expect("Failed read"); if len == 0 { return None; } self.buf_iter = unsafe { let slice = str::from_utf8_unchecked(&self.buf_str); std::mem::transmute(slice.split_ascii_whitespace()) } } }
+    pub fn line(&mut self) -> Option<String> { let mut input = String::new(); let len = self.reader.read_line(&mut input).expect("Failed read"); match len { 0 => None, _ => { Self::trim_newline(&mut input); Some(input) } } }
+}
+fn main() { let (stdin, stdout) = (io::stdin(), io::stdout()); let mut scan = UnsafeScanner::new(stdin.lock()); let mut out = io::BufWriter::new(stdout.lock()); solve(&mut scan, &mut out); }
+
+fn solve<R: io::BufRead, W: io::Write>(scan: &mut UnsafeScanner<R>, out: &mut W) {
+    let mut accum = 0;
+    loop {
+        let line = scan.line();
+        if line.is_none() { return; }
+        let line = line.unwrap();
+        let len = line.len();
+
+        if len > 0 {
+            let iter = line.chars().into_iter();
+            let asterisks = iter.filter(|c| *c =='*').count();
+            for _ in 0..len-accum-asterisks { print!("."); }
+            for _ in 0..asterisks { print!("*"); }
+            for _ in 0..accum { print!("."); }
+            accum += asterisks;
+        } else {
+            accum = 0;
+        }
+        println!();
+    }
+}
