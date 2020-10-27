0cdc4eddd9b24a4008de9eed163da4ef33299ab5
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2020-10-27 10:23:14 +0700

    Add 5.1 mali

---
+++ Kattis/mali/src/main.rs
@@ -0,0 +1,47 @@
+use std::io;
+use std::str;
+use std::convert::TryFrom;
+
+pub struct UnsafeScanner<R> { reader: R, buf_str: Vec<u8>, buf_iter: str::SplitAsciiWhitespace<'static>, }
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self { Self { reader, buf_str: vec![], buf_iter: "".split_ascii_whitespace(), } }
+    pub fn trim_newline(s: &mut String) { if s.ends_with('\n') { s.pop(); if s.ends_with('\r') { s.pop(); } } }
+    pub fn token<T: str::FromStr>(&mut self) -> Option<T> { loop { if let Some(token) = self.buf_iter.next() { return Some(token.parse().ok().expect("Failed parse")); } self.buf_str.clear(); let len = self.reader .read_until(b'\n', &mut self.buf_str) .expect("Failed read"); if len == 0 { return None; } self.buf_iter = unsafe { let slice = str::from_utf8_unchecked(&self.buf_str); std::mem::transmute(slice.split_ascii_whitespace()) } } }
+    pub fn line(&mut self) -> Option<String> { let mut input = String::new(); let len = self.reader.read_line(&mut input).expect("Failed read"); match len { 0 => None, _ => { Self::trim_newline(&mut input); Some(input) } } }
+}
+fn main() { let (stdin, stdout) = (io::stdin(), io::stdout()); let mut scan = UnsafeScanner::new(stdin.lock()); let mut out = io::BufWriter::new(stdout.lock()); solve(&mut scan, &mut out); }
+
+fn solve<R: io::BufRead, W: io::Write>(scan: &mut UnsafeScanner<R>, out: &mut W) {
+    let n = scan.token::<usize>().unwrap();
+    let mut counts = vec![vec![0;101];2];
+    for i in 0..n {
+        let a = scan.token::<usize>().unwrap();
+        let b = scan.token::<usize>().unwrap();
+
+        counts[0][a] += 1;
+        counts[1][b] += 1;
+
+        let mut sum = 0;
+        let mut i = 1;
+        let mut j = 100;
+        let mut rem_i = 0;
+        let mut rem_j = 0;
+
+        while i <= 100 && j >= 1 {
+            if counts[0][i] == 0 { i += 1; continue; }
+            if counts[1][j] == 0 { j -= 1; continue; }
+            sum = (i + j).max(sum);
+
+            if rem_i == 0 { rem_i = counts[0][i] };
+            if rem_j == 0 { rem_j = counts[1][j] };
+
+            let min = rem_i.min(rem_j);
+            if min == rem_j { j -= 1; }
+            if min == rem_i { i += 1; }
+            rem_i -= min;
+            rem_j -= min;
+        }
+
+        writeln!(out, "{}", sum).unwrap();
+    }
+}
