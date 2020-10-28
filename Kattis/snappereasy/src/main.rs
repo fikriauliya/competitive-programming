3dc13a1a9e19201b137d4568b318c0e9e499d963
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2020-10-29 05:08:37 +0700

    Add 2.7 snappereasy

---
+++ Kattis/snappereasy/src/main.rs
@@ -0,0 +1,26 @@
+use std::io;
+use std::str;
+use std::convert::TryFrom;
+
+pub struct UnsafeScanner<R> { reader: R, buf_str: Vec<u8>, buf_iter: str::SplitAsciiWhitespace<'static>, }
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self { Self { reader, buf_str: vec![], buf_iter: "".split_ascii_whitespace(), } }
+    pub fn trim_newline(s: &mut String) { if s.ends_with('\n') { s.pop(); if s.ends_with('\r') { s.pop(); } } }
+    pub fn token<T: str::FromStr>(&mut self) -> Option<T> { loop { if let Some(token) = self.buf_iter.next() { return Some(token.parse().ok().expect("Failed parse")); } self.buf_str.clear(); let len = self.reader .read_until(b'\n', &mut self.buf_str) .expect("Failed read"); if len == 0 { return None; } self.buf_iter = unsafe { let slice = str::from_utf8_unchecked(&self.buf_str); std::mem::transmute(slice.split_ascii_whitespace()) } } }
+    pub fn line(&mut self) -> Option<String> { let mut input = String::new(); let len = self.reader.read_line(&mut input).expect("Failed read"); match len { 0 => None, _ => { Self::trim_newline(&mut input); Some(input) } } }
+}
+fn main() { let (stdin, stdout) = (io::stdin(), io::stdout()); let mut scan = UnsafeScanner::new(stdin.lock()); let mut out = io::BufWriter::new(stdout.lock()); solve(&mut scan, &mut out); }
+
+fn solve<R: io::BufRead, W: io::Write>(scan: &mut UnsafeScanner<R>, out: &mut W) {
+    let t = scan.token::<usize>().unwrap();
+    for tt in 0..t {
+        let n = scan.token::<u32>().unwrap();
+        let mut k = scan.token::<u32>().unwrap();
+        let mask = (1 << n) - 1;
+        if k > mask {
+            k = mask & k;
+        }
+        let res = if mask == k { "ON" } else { "OFF" };
+        writeln!(out, "Case #{}: {}", tt+1, res).unwrap();
+    }
+}
