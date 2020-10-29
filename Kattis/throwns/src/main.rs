e81f55d9ee5c086e8e60939cb6551c2f2c06a1c6
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2020-10-30 05:13:51 +0700

    Add 2.7 throwns

---
+++ Kattis/throwns/src/main.rs
@@ -0,0 +1,38 @@
+use std::io;
+use std::str;
+use std::convert::TryFrom;
+
+pub struct UnsafeScanner<R> { reader: R, buf_str: Vec<u8>, buf_iter: str::SplitAsciiWhitespace<'static>, }
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self { Self { reader, buf_str: vec![], buf_iter: "".split_ascii_whitespace(), } }
+    pub fn trim_newline(s: &mut String) { if s.ends_with('\n') { s.pop(); if s.ends_with('\r') { s.pop(); } } }
+    pub fn token<T: str::FromStr>(&mut self) -> Option<T> { loop { if let Some(token) = self.buf_iter.next() { return Some(token.parse().ok().expect("Failed parse")); } self.buf_str.clear(); let len = self.reader .read_until(b'\n', &mut self.buf_str) .expect("Failed read"); if len == 0 { return None; } self.buf_iter = unsafe { let slice = str::from_utf8_unchecked(&self.buf_str); std::mem::transmute(slice.split_ascii_whitespace()) } } }
+    pub fn line(&mut self) -> Option<String> { let mut input = String::new(); let len = self.reader.read_line(&mut input).expect("Failed read"); match len { 0 => None, _ => { Self::trim_newline(&mut input); Some(input) } } }
+}
+fn main() { let (stdin, stdout) = (io::stdin(), io::stdout()); let mut scan = UnsafeScanner::new(stdin.lock()); let mut out = io::BufWriter::new(stdout.lock()); solve(&mut scan, &mut out); }
+
+fn solve<R: io::BufRead, W: io::Write>(scan: &mut UnsafeScanner<R>, out: &mut W) {
+    let n = scan.token::<usize>().unwrap();
+    let _ = scan.token::<usize>().unwrap();
+    let mut stack = Vec::new();
+    loop {
+        let token = scan.token::<String>();
+        if token.is_none() { break; }
+        let token = token.unwrap();
+        let token = token.trim();
+        match token {
+            "undo" => {
+                let mut step = scan.token::<usize>().unwrap();
+                while step > 0 { stack.pop(); step -= 1 }
+            }
+            "" => break,
+            _ => { stack.push(token.parse::<i32>().unwrap()) }
+        }
+    }
+    let mut pos = 0;
+    while let Some(step) = stack.pop() {
+        pos += step;
+        pos = pos.rem_euclid(n as i32);
+    }
+    println!("{}", pos);
+}
