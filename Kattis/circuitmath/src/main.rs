4678eb73cfcd9ec4c20b52dfab7fa1745ad23856
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2020-10-30 06:29:18 +0700

    Add 2.4 circuitmath

---
+++ Kattis/circuitmath/src/main.rs
@@ -0,0 +1,59 @@
+use std::io;
+use std::str;
+use std::convert::TryFrom;
+
+pub struct UnsafeScanner<R> { reader: R, buf_str: Vec<u8>, buf_iter: str::SplitAsciiWhitespace<'static>, }
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self { Self { reader, buf_str: vec![], buf_iter: "".split_ascii_whitespace(), } }
+    pub fn trim_newline(s: &mut String) { if s.ends_with('\n') { s.pop(); if s.ends_with('\r') { s.pop(); } } }
+    pub fn token<T: str::FromStr>(&mut self) -> Option<T> { loop { if let Some(token) = self.buf_iter.next() { return Some(token.parse().ok().expect("Failed parse")); } self.buf_str.clear(); let len = self.reader .read_until(b'\n', &mut self.buf_str) .expect("Failed read"); if len == 0 { return None; } self.buf_iter = unsafe { let slice = str::from_utf8_unchecked(&self.buf_str); std::mem::transmute(slice.split_ascii_whitespace()) } } }
+    pub fn line(&mut self) -> Option<String> { let mut input = String::new(); let len = self.reader.read_line(&mut input).expect("Failed read"); match len { 0 => None, _ => { Self::trim_newline(&mut input); Some(input) } } }
+}
+fn main() { let (stdin, stdout) = (io::stdin(), io::stdout()); let mut scan = UnsafeScanner::new(stdin.lock()); let mut out = io::BufWriter::new(stdout.lock()); solve(&mut scan, &mut out); }
+
+fn solve<R: io::BufRead, W: io::Write>(scan: &mut UnsafeScanner<R>, out: &mut W) {
+    let _ = scan.token::<usize>().unwrap();
+    let mut values = Vec::new();
+    let line = scan.line().unwrap();
+    let line = line.split_ascii_whitespace();
+    for c in line {
+        let c = match c {
+            "T" => true,
+            "F" => false,
+            _ => panic!()
+        };
+        values.push(c);
+    }
+
+    let line = scan.line().unwrap();
+    let line= line.split_ascii_whitespace();
+    let mut operand = Vec::new();
+    for c in line {
+        let c = c.chars().next().unwrap();
+        match c {
+            'A'..='Z' => {
+                let val = values[(c as u8 - 'A' as u8) as usize];
+                operand.push(val);
+            },
+            '+' => {
+                let a = operand.pop().unwrap();
+                let b = operand.pop().unwrap();
+                operand.push(a | b);
+            },
+            '*' => {
+                let a = operand.pop().unwrap();
+                let b = operand.pop().unwrap();
+                operand.push(a & b);
+            },
+            '-' => {
+                let a = operand.pop().unwrap();
+                operand.push(!a);
+            }
+            _ => panic!()
+        }
+    }
+    match operand.pop().unwrap() {
+        false => println!("F"),
+        true => println!("T"),
+    }
+}
