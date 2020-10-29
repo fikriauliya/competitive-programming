f638b1b0d45adab86a4a4fc52cf661265dc330df
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2020-10-30 06:14:42 +0700

    Add 1.8 delimitersoup

---
+++ Kattis/delimitersoup/src/main.rs
@@ -0,0 +1,49 @@
+use std::io;
+use std::str;
+use std::convert::TryFrom;
+
+pub struct UnsafeScanner<R> { reader: R, buf_str: Vec<u8>, buf_iter: str::SplitAsciiWhitespace<'static>, }
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self { Self { reader, buf_str: vec![], buf_iter: "".split_ascii_whitespace(), } }
+    pub fn trim_newline(s: &mut String) { if s.ends_with('\n') { s.pop(); if s.ends_with('\r') { s.pop(); } } }
+    pub fn token<T: str::FromStr>(&mut self) -> Option<T> { loop { if let Some(token) = self.buf_iter.next() { return Some(token.parse().ok().expect("Failed parse")); } self.buf_str.clear(); let len = self.reader .read_until(b'\n', &mut self.buf_str) .expect("Failed read"); if len == 0 { return None; } self.buf_iter = unsafe { let slice = str::from_utf8_unchecked(&self.buf_str); std::mem::transmute(slice.split_ascii_whitespace()) } } }
+    pub fn line(&mut self) -> Option<String> { let mut input = String::new(); let len = self.reader.read_line(&mut input).expect("Failed read"); match len { 0 => None, _ => { Self::trim_newline(&mut input); Some(input) } } }
+}
+fn main() { let (stdin, stdout) = (io::stdin(), io::stdout()); let mut scan = UnsafeScanner::new(stdin.lock()); let mut out = io::BufWriter::new(stdout.lock()); solve(&mut scan, &mut out); }
+
+fn solve<R: io::BufRead, W: io::Write>(scan: &mut UnsafeScanner<R>, out: &mut W) {
+    let _ = scan.line();
+    let s = scan.line().unwrap();
+    let s = s.chars();
+    let mut stack = Vec::new();
+    let mut err_pos = None;
+    let mut err_char = None;
+    for (i, c) in s.enumerate() {
+        match c {
+            '(' | '[' | '{' => { stack.push(c) },
+            ')' | ']' | '}' => {
+                let t = stack.pop();
+                let is_err = match t {
+                    None => true,
+                    Some(t) => {
+                        match (c, t) {
+                            (')', '(') | ('}', '{')  | (']', '[') => false,
+                            _ => true,
+                        }
+                    }
+                };
+                if is_err {
+                    err_char = Some(c);
+                    err_pos = Some(i);
+                    break;
+                }
+            }
+            ' ' => continue,
+            _ => panic!()
+        }
+    }
+    match (err_char, err_pos) {
+        (Some(c), Some(i)) => println!("{} {}", c, i),
+        _ => println!("ok so far")
+    }
+}
