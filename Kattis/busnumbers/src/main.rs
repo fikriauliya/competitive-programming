2e165b7affbca153a05c1f64a88748c32e50b0be
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2020-11-01 16:33:34 +0700

    Add 2.7 busnumbers

---
+++ Kattis/busnumbers/src/main.rs
@@ -0,0 +1,38 @@
+use std::io;
+use std::str;
+use std::convert::TryFrom;
+
+pub struct UnsafeScanner<R> { reader: R, buf_str: Vec<u8>, buf_iter: str::SplitAsciiWhitespace<'static>, }
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self { Self { reader, buf_str: vec![], buf_iter: "".split_ascii_whitespace(), } }
+    pub fn trim_newline(s: &mut String) { if s.ends_with('\n') { s.pop(); if s.ends_with('\r') { s.pop(); } } }
+    pub fn token<T: str::FromStr>(&mut self) -> Option<T> { loop { if let Some(token) = self.buf_iter.next() { return Some(token.parse().ok().expect("Failed parse")); } self.buf_str.clear(); let len = self.reader .read_until(b'\n', &mut self.buf_str) .expect("Failed read"); if len == 0 { return None; } self.buf_iter = unsafe { let slice = str::from_utf8_unchecked(&self.buf_str); std::mem::transmute(slice.split_ascii_whitespace()) } } }
+    pub fn line(&mut self) -> Option<String> { let mut input = String::new(); let len = self.reader.read_line(&mut input).expect("Failed read"); match len { 0 => None, _ => { Self::trim_newline(&mut input); Some(input) } } }
+}
+fn main() { let (stdin, stdout) = (io::stdin(), io::stdout()); let mut scan = UnsafeScanner::new(stdin.lock()); let mut out = io::BufWriter::new(stdout.lock()); solve(&mut scan, &mut out); }
+
+fn solve<R: io::BufRead, W: io::Write>(scan: &mut UnsafeScanner<R>, out: &mut W) {
+    let n = scan.token::<usize>().unwrap();
+    let mut v = vec![false;1001];
+    for i in 0..n {
+        let num = scan.token::<usize>().unwrap();
+        v[num] = true;
+    }
+    let mut i = 1;
+    let mut first = true;
+    while i < v.len() {
+        if v[i] {
+            let mut j = i;
+            while j < v.len() && v[j] { j += 1 };
+            if !first { print!(" "); }
+            print!("{}", i);
+            if j >= i + 3 { 
+                print!("-{}", j-1);
+                i = j - 1;
+            };
+            first = false;
+        }
+        i += 1;
+    }
+    println!()
+}
