ff57f72b8b185f53295916d45923e0ab6f7ece19
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2020-11-17 08:24:33 +0700

    Add 2.9 telephones

---
+++ Kattis/telephones/src/main.rs
@@ -0,0 +1,46 @@
+use std::io; use std::str;
+#[allow(unused_imports)] use std::convert::TryFrom;
+#[allow(unused_imports)] use std::cmp::{Reverse, Ordering};
+#[allow(unused_imports)] use std::collections::{LinkedList, HashSet, HashMap, BinaryHeap, BTreeSet, BTreeMap};
+
+pub struct UnsafeScanner<R> { reader: R, buf_str: Vec<u8>, buf_iter: str::SplitAsciiWhitespace<'static>, } impl<R: io::BufRead> UnsafeScanner<R> { pub fn new(reader: R) -> Self { Self { reader, buf_str: vec![], buf_iter: "".split_ascii_whitespace(), } } pub fn trim_newline(s: &mut String) { if s.ends_with('\n') { s.pop(); if s.ends_with('\r') { s.pop(); } } } pub fn token<T: str::FromStr>(&mut self) -> Option<T> { loop { if let Some(token) = self.buf_iter.next() { return Some(token.parse().ok().expect("Failed parse")); } self.buf_str.clear(); let len = self.reader .read_until(b'\n', &mut self.buf_str) .expect("Failed read"); if len == 0 { return None; } self.buf_iter = unsafe { let slice = str::from_utf8_unchecked(&self.buf_str); std::mem::transmute(slice.split_ascii_whitespace()) } } } pub fn line(&mut self) -> Option<String> { let mut input = String::new(); let len = self.reader.read_line(&mut input).expect("Failed read"); match len { 0 => None, _ => { Self::trim_newline(&mut input); Some(input) } } } } fn main() { let (stdin, stdout) = (io::stdin(), io::stdout()); let mut scan = UnsafeScanner::new(stdin.lock()); let mut out = io::BufWriter::new(stdout.lock()); solve(&mut scan, &mut out); }
+
+#[derive(Copy, Clone)]
+struct Call {
+    src: usize,
+    dest: usize,
+    start: usize,
+    end: usize
+}
+
+fn solve<R: io::BufRead, W: io::Write>(scan: &mut UnsafeScanner<R>, out: &mut W) {
+    loop {
+        let n = scan.token::<usize>().unwrap();
+        let m = scan.token::<usize>().unwrap();
+        if n == 0 && m == 0 { break; }
+        let mut calls = Vec::with_capacity(n);
+        for _ in 0..n {
+            let src = scan.token::<usize>().unwrap();
+            let dest = scan.token::<usize>().unwrap();
+            let start = scan.token::<usize>().unwrap();
+            let duration = scan.token::<usize>().unwrap();
+            let end = start + duration;
+            calls.push(Call{src, dest, start, end});
+        }
+        for _ in 0..m {
+            let start = scan.token::<usize>().unwrap();
+            let duration = scan.token::<usize>().unwrap();
+            let end = start + duration;
+            let mut res = 0;
+            for i in 0..n {
+                let c = calls[i];
+                if start >= c.start && start < c.end {
+                    res += 1;
+                } else if start < c.start && end > c.start {
+                    res += 1;
+                }
+            }
+            writeln!(out, "{}", res);
+        }
+    }
+}
