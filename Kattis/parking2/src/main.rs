07327b556f90fd618b3f2f7bd4c76c970d916ab3
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2021-02-28 15:43:55 +0700

    Add 1.3 parking2

---
+++ Kattis/parking2/src/main.rs
@@ -0,0 +1,31 @@
+use std::io; use std::str;
+#[allow(unused_imports)] use std::convert::TryFrom;
+#[allow(unused_imports)] use std::cmp::{Reverse, Ordering};
+#[allow(unused_imports)] use std::collections::{LinkedList, HashSet, HashMap, BinaryHeap, BTreeSet, BTreeMap};
+
+pub struct UnsafeScanner<R> { reader: R, buf_str: Vec<u8>, buf_iter: str::SplitAsciiWhitespace<'static>, } impl<R: io::BufRead> UnsafeScanner<R> { pub fn new(reader: R) -> Self { Self { reader, buf_str: vec![], buf_iter: "".split_ascii_whitespace(), } } pub fn trim_newline(s: &mut String) { if s.ends_with('\n') { s.pop(); if s.ends_with('\r') { s.pop(); } } } pub fn token<T: str::FromStr>(&mut self) -> Option<T> { loop { if let Some(token) = self.buf_iter.next() { return Some(token.parse().ok().expect("Failed parse")); } self.buf_str.clear(); let len = self.reader .read_until(b'\n', &mut self.buf_str) .expect("Failed read"); if len == 0 { return None; } self.buf_iter = unsafe { let slice = str::from_utf8_unchecked(&self.buf_str); std::mem::transmute(slice.split_ascii_whitespace()) } } } pub fn line(&mut self) -> Option<String> { let mut input = String::new(); let len = self.reader.read_line(&mut input).expect("Failed read"); match len { 0 => None, _ => { Self::trim_newline(&mut input); Some(input) } } } } fn main() { let (stdin, stdout) = (io::stdin(), io::stdout()); let mut scan = UnsafeScanner::new(stdin.lock()); let mut out = io::BufWriter::new(stdout.lock()); solve(&mut scan, &mut out); }
+
+fn solve<R: io::BufRead, W: io::Write>(scan: &mut UnsafeScanner<R>, out: &mut W) {
+    let t = scan.token::<usize>().unwrap();
+    for _ in 0..t {
+        let n = scan.token::<usize>().unwrap();
+        let mut xs = Vec::new();
+        for _ in 0..n {
+            let x = scan.token::<i32>().unwrap();
+            xs.push(x);
+        }
+        xs.sort();
+        let mut res = std::i32::MAX;
+        for i in 0..100 {
+            let mut loc = i;
+            let mut cur_res = 0;
+            for x in &xs {
+                cur_res += (loc - x).abs();
+                loc = *x;
+            }
+            cur_res += (loc - i).abs();
+            res = res.min(cur_res);
+        }
+        writeln!(out, "{}", res);
+    }
+}
