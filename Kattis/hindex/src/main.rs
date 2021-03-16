6a92acc1a08adc6f5739270f2318cfca739159c0
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2021-03-17 06:21:55 +0700

    Add 3.2 hindex

---
+++ Kattis/hindex/src/main.rs
@@ -0,0 +1,28 @@
+use std::io; use std::str;
+#[allow(unused_imports)] use std::convert::TryFrom;
+#[allow(unused_imports)] use std::cmp::{Reverse, Ordering};
+#[allow(unused_imports)] use std::collections::{LinkedList, HashSet, HashMap, BinaryHeap, BTreeSet, BTreeMap};
+
+pub struct UnsafeScanner<R> { reader: R, buf_str: Vec<u8>, buf_iter: str::SplitAsciiWhitespace<'static>, } impl<R: io::BufRead> UnsafeScanner<R> { pub fn new(reader: R) -> Self { Self { reader, buf_str: vec![], buf_iter: "".split_ascii_whitespace(), } } pub fn trim_newline(s: &mut String) { if s.ends_with('\n') { s.pop(); if s.ends_with('\r') { s.pop(); } } } pub fn token<T: str::FromStr>(&mut self) -> Option<T> { loop { if let Some(token) = self.buf_iter.next() { return Some(token.parse().ok().expect("Failed parse")); } self.buf_str.clear(); let len = self.reader .read_until(b'\n', &mut self.buf_str) .expect("Failed read"); if len == 0 { return None; } self.buf_iter = unsafe { let slice = str::from_utf8_unchecked(&self.buf_str); std::mem::transmute(slice.split_ascii_whitespace()) } } } pub fn line(&mut self) -> Option<String> { let mut input = String::new(); let len = self.reader.read_line(&mut input).expect("Failed read"); match len { 0 => None, _ => { Self::trim_newline(&mut input); Some(input) } } } } fn main() { let (stdin, stdout) = (io::stdin(), io::stdout()); let mut scan = UnsafeScanner::new(stdin.lock()); let mut out = io::BufWriter::new(stdout.lock()); solve(&mut scan, &mut out); }
+
+fn solve<R: io::BufRead, W: io::Write>(scan: &mut UnsafeScanner<R>, out: &mut W) {
+    let n = scan.token::<usize>().unwrap();
+    let mut cits = Vec::with_capacity(n);
+    for _ in 0..n {
+        let cit = scan.token::<usize>().unwrap();
+        cits.push(cit);
+    }
+    cits.sort();
+    let mut res = 0;
+    for h in 0..=cits[n-1] {
+        let i = cits.binary_search(&h);
+        let mut i = match i {
+            Ok(i) => {i},
+            Err(i) => {i},
+        };
+        while i > 0 && cits[i] == cits[i-1] { i-=1 }
+        if h <= cits.len() - i { res = h}
+        else { break; }
+    }
+    writeln!(out, "{}", res).unwrap();
+}
